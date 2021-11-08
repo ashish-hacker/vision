5fa09dfa4366a1a7a3ae73833866644f2b3b7653
Mitesh Vasani <53206098+mtsvasani@users.noreply.github.com>
2021-11-08 12:26:41 +0530

    implemented image show

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import streamlit as st
 import re
 import sys
 import os
-import pyttsx3
 from streamlit.cli import main
+import pyttsx3
 from PIL import Image
 
-engine = pyttsx3.init()
-
+# engine = pyttsx3.init()
 
 
 def save_uploaded_file(uploaded_file):
@@ -23,28 +22,31 @@ def main():
     st.set_option('deprecation.showfileUploaderEncoding', False)
     st.markdown("<h1 style='text-align: center; color: White;background-color:#e84343'>Caption Generator</h1>", unsafe_allow_html=True)
     
-    st.markdown("<h4 style='text-align: center; color: Black;'>Upload Image</h4>", unsafe_allow_html=True)
+    st.markdown("<h4 style='text-align: center; color: Black;'>describing the content of the image</h4><br><br>", unsafe_allow_html=True)
     st.sidebar.header("What is this Project about?")
     st.sidebar.text("It a Web app that would help the user generate caption from an image. In other words it shows the ability of computers to visualize images conceptually.")
     st.sidebar.header("What tools where used to make this?")
-    st.sidebar.text("The Model was made using a dataset from Google along with using Google colab to train the model. We made use of Deep Neural Networks to train the model.")
+    st.sidebar.text("The Model was trained using the Flickr8k dataset on google colab. We made use of Deep Neural Networks to train the model.")
     
 
-    uploaded_file = st.file_uploader("drop the image here")
+    uploaded_file = st.file_uploader("Select image to generate caption")
     
-    caption = ""
+
     if st.button('Generate Caption'): #making and printing our prediction
         if uploaded_file:
             img = Image.open(uploaded_file)
+            st.image(uploaded_file, width=500)
             caption = generate_caption(img)
-            st.markdown(caption[5:-3])
-            engine.say(caption[5:-3])
-            engine.runAndWait()
-        # st.success('The Probability of getting admission is {}'.format(1))
+            # st.image(uploaded_file, width=264)
+            st.markdown(f"<h3 style='text-align: left; color: Black;'>{caption[5:-3]}</h3>", unsafe_allow_html=True)
+            # engine.say(caption[5:-3])
+            # engine.runAndWait()
+        else:
+            st.error('Select an image to generate caption')
+
 
 
 
 if __name__ =='__main__':
     main()
-    # sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
-    # sys.exit(main())
+    
